0dd373ad726896450eb9a4c43a9b127b4a92f96c
ravijar <ravijarandinu@gmail.com>
2024-06-21 00:14:59 +0530

    added navigation

--- App.js
+++ App.js
@@ -1,20 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './Navigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className="text-red-500 text-4xl">Hello</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Navigation />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Navigation.js
@@ -0,0 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './screens/Home';
+import ChooseLocation from './screens/ChooseLocation';
+import ChoosePeople from './screens/ChoosePeople';
+import Vote from './screens/Vote';
+
+const Stack = createNativeStackNavigator();
+
+export default function Navigation() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="ChooseLocation" component={ChooseLocation} />
+                <Stack.Screen name="ChoosePeople" component={ChoosePeople} />
+                <Stack.Screen name="Vote" component={Vote} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
