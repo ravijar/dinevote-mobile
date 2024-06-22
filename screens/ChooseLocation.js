983ceff116a7174d923cedc7cc3ea5c9f1dd4c58
ravijar <ravijarandinu@gmail.com>
2024-06-22 05:58:37 +0530

    added radius input

--- screens/ChooseLocation.js
+++ screens/ChooseLocation.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, TouchableWithoutFeedback, Keyboard, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import MapView, { Circle, Marker } from 'react-native-maps'
 import { themeColors } from '../theme'
@@ -6,6 +6,7 @@ import { StatusBar } from 'expo-status-bar'
 import BottomMenu from '../components/BottomMenu'
 import Avatar from '../components/Avatar'
 import { locationTypes } from '../constants'
+import { MaterialIcons } from '@expo/vector-icons'
 
 export default function ChooseLocation() {
 
@@ -18,67 +19,135 @@ export default function ChooseLocation() {
   }
 
   const [selectedlocationType, setSelectedLocationType] = useState("restaurant")
+  const [radius, setRadius] = useState(0);
+  const [radiusInput, setRadiusInput] = useState("0");
 
   const locationTypeHandler = (type) => {
     setSelectedLocationType(type)
   }
 
+  const radiusInputHandler = (text) => {
+    setRadiusInput(text)
+  };
+
+  const radiusValueHandler = () => {
+    const parsedRadius = parseFloat(radiusInput);
+    if (!isNaN(parsedRadius) && parsedRadius >= 0 && parsedRadius <= 100) {
+      const roundedRadius = parseFloat(parsedRadius.toFixed(1));
+      setRadius(roundedRadius)
+    }
+  }
+
+  const incrementRadius = () => {
+    setRadius(prevRadius => {
+      const newRadius = Math.min(prevRadius + 0.5, 100)
+      const roundedRadius = parseFloat(newRadius.toFixed(1))
+      setRadiusInput(roundedRadius.toString())
+      return roundedRadius
+    })
+  }
+
+  const decrementRadius = () => {
+    setRadius(prevRadius => {
+      const newRadius = Math.max(prevRadius - 0.5, 0)
+      const roundedRadius = parseFloat(newRadius.toFixed(1))
+      setRadiusInput(roundedRadius.toString())
+      return roundedRadius
+    })
+  }
+
   return (
-    <View className="flex-1">
-      <StatusBar style="dark" />
-      {/* map */}
-      <MapView
-        initialRegion={{
-          latitude: locationData.latitute,
-          longitude: locationData.longitude,
-          latitudeDelta: locationData.latitudeDelta,
-          longitudeDelta: locationData.longitudeDelta
-        }}
-        className="flex-1"
-        mapType='standard'
-      >
-        <Marker
-          coordinate={{
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss()
+      radiusValueHandler()
+    }}>
+      <View className="flex-1">
+        <StatusBar style="dark" />
+        {/* map */}
+        <MapView
+          initialRegion={{
             latitude: locationData.latitute,
-            longitude: locationData.longitude
+            longitude: locationData.longitude,
+            latitudeDelta: locationData.latitudeDelta,
+            longitudeDelta: locationData.longitudeDelta
           }}
-          pinColor={themeColors.bgColor(1)}
-          title="Your Location"
-          description={`${locationData.latitute}, ${locationData.longitude}`}
-        />
-        <Circle
-          center={{
-            latitude: locationData.latitute,
-            longitude: locationData.longitude
-          }}
-          radius={locationData.radius * 1000}
-          strokeColor={themeColors.bgColor(1)}
-          fillColor={themeColors.bgColor(0.3)}
-        />
-      </MapView>
+          className="flex-1"
+          mapType='standard'
+        >
+          <Marker
+            coordinate={{
+              latitude: locationData.latitute,
+              longitude: locationData.longitude
+            }}
+            pinColor={themeColors.bgColor(1)}
+            title="Your Location"
+            description={`${locationData.latitute}, ${locationData.longitude}`}
+          />
+          <Circle
+            center={{
+              latitude: locationData.latitute,
+              longitude: locationData.longitude
+            }}
+            radius={radius * 1000}
+            strokeColor={themeColors.bgColor(1)}
+            fillColor={themeColors.bgColor(0.3)}
+          />
+        </MapView>
 
-      {/* bottom menu */}
-      <BottomMenu>
-        <View className="flex-row justify-evenly">
-          {Object.values(locationTypes).map(locationType => {
-            let isSelected = locationType.key === selectedlocationType
-            let textClass = isSelected ? "font-extrabold text-gray-800" : "font-semibold text-gray-500"
+        {/* bottom menu */}
+        <BottomMenu>
+          <Text className="text-xl font-bold text-center text-gray-600 mb-4">Map Options</Text>
+          <View className="flex-row mb-5">
+            <View className="flex-row basis-3/4 items-center">
+              <Text className="text-lg font-semibold text-center text-gray-500 ml-3">Radius (km): </Text>
 
-            return (
-              <View key={locationType.key}>
-                <Avatar
-                  key={locationType.key}
-                  image={locationType.image}
-                  size={40}
-                  selected={isSelected}
-                  onPressHandler={() => locationTypeHandler(locationType.key)}
+              {/* radius input */}
+              <View className="flex-row flex-1 rounded-full border border-gray-300 p-1 ml-2">
+                <TouchableOpacity onPress={decrementRadius}>
+                  <View className="rounded-l-full p-2 justify-start" style={{ backgroundColor: themeColors.bgColor(0.2) }}>
+                    <MaterialIcons name="remove" />
+                  </View>
+                </TouchableOpacity>
+                <TextInput
+                  className="text-lg font-semibold text-center text-gray-800 flex-1"
+                  keyboardType="numeric"
+                  cursorColor={themeColors.bgColor(0.8)}
+                  onChangeText={radiusInputHandler}
+                  onSubmitEditing={radiusValueHandler}
+                  returnKeyType="done"
+                  value={radiusInput.toString()}
                 />
-                <Text className={"text-xs text-center mt-1 " + textClass}>{locationType.displayName}</Text>
+                <TouchableOpacity onPress={incrementRadius}>
+                  <View className="rounded-r-full p-2 justify-end" style={{ backgroundColor: themeColors.bgColor(0.2) }}>
+                    <MaterialIcons name="add" />
+                  </View>
+                </TouchableOpacity>
               </View>
-            )
-          })}
-        </View>
-      </BottomMenu>
-    </View>
+            </View>
+          </View>
+
+          <Text className="text-xl font-bold text-center text-gray-600 mb-4">Choose Location Type</Text>
+          <View className="flex-row justify-evenly">
+            {Object.values(locationTypes).map(locationType => {
+              let isSelected = locationType.key === selectedlocationType
+              let textClass = isSelected ? "font-extrabold text-gray-800" : "font-semibold text-gray-500"
+
+              return (
+                <View key={locationType.key}>
+                  <Avatar
+                    key={locationType.key}
+                    image={locationType.image}
+                    size={40}
+                    selected={isSelected}
+                    onPressHandler={() => locationTypeHandler(locationType.key)}
+                  />
+                  <Text className={"text-xs text-center mt-1 " + textClass}>{locationType.displayName}</Text>
+                </View>
+              )
+            })}
+          </View>
+        </BottomMenu>
+      </View>
+    </TouchableWithoutFeedback>
   )
 }
